be70aa8ed15ef819776c7945f122a5fafc7ed31d
강석민 <dol2156@gmail.com>
2023-12-15 11:13:56 +0900

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# 📌 Summary
+- Title : Undefined
+- Front-end framework used : Undefined
+
+
+# 📌 Run Local Test Server Command
+```
+npm run dev
+```
+
+# 📌 Stage Server
+- Undefined
+
+
+# 📌 Real Server
+- Undefined
---
+++ src/components/meta/Head_1.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Head_1() {
+  return (
+    <>
+      <head>
+        <link rel="preload" href="/font/SUIT-Variable.woff2" as="font" type="font/woff2" crossOrigin="true" />
+        <link rel="preload" href="/font/Montserrat-VF.woff2" as="font" type="font/woff2" crossOrigin="true" />
+      </head>
+    </>
+  );
+}
