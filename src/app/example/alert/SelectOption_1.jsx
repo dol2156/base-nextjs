48cac94bc48d0b92eebcbfbbd484c85a498f2df5
강석민 <dol2156@gmail.com>
2023-12-18 12:00:46 +0900

    ddd

---
+++ src/app/example/alert/SelectOption_1.jsx
@@ -0,0 +1,20 @@
+'use client';
+import { layerControl } from '@/js/common';
+
+export default function SelectOption_1() {
+  return (
+    <>
+      <div className="SelectOption_1 w-[302px]">
+        <div className="Body">
+          <div className="Title">프로필 사진 설정</div>
+          <button className="Button">사진 보관함</button>
+          <button className="Button">사진찍기</button>
+          <button className="Button">기본 이미지로 변경 </button>
+          <button className="Button" onClick={layerControl.Off}>
+            취소
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/example/modal/page.js
+++ src/app/example/modal/page.js
@@ -1,18 +1,35 @@
 'use client';
 
 import Confirm_1 from '@/app/example/alert/Confirm_1';
+import SelectOption_1 from '@/app/example/alert/SelectOption_1';
 import ToolTip_1 from '@/components/layerpop/ToolTip_1';
 import { modalControl } from '@/js/common';
 import { useRef } from 'react';
 
 export default function Page() {
-  const ref_layerpop = useRef();
+  const ref_tooltip = useRef();
+  const ref_confirm = useRef();
+  const ref_selecttoption = useRef();
 
-  function onOpenLayerPop() {
+  function onOpenTooltip() {
     modalControl.On();
 
-    const $layerpop = ref_layerpop.current;
-    $layerpop.classList.add('On');
+    const $target = ref_tooltip.current;
+    $target.classList.add('On');
+  }
+
+  function onOpenConfirm() {
+    modalControl.On();
+
+    const $target = ref_confirm.current;
+    $target.classList.add('On');
+  }
+
+  function onOpenSelectOption() {
+    modalControl.On();
+
+    const $target = ref_selecttoption.current;
+    $target.classList.add('On');
   }
 
   return (
@@ -22,21 +39,28 @@ export default function Page() {
         <section className={`HBox`}>
           <div className="Inner">
             <h2 className="H2 mb-[10px]">AAA</h2>
-            <button className={`Button_1`} onClick={onOpenLayerPop}>
+            <button className={`Button_1`} onClick={onOpenTooltip}>
               Open ToolTip_1
             </button>
             <br />
-            <button className={`Button_1`} onClick={onOpenLayerPop}>
+            <button className={`Button_1`} onClick={onOpenConfirm}>
               Open Confirm_1
+            </button>
+            <br />
+            <button className={`Button_1`} onClick={onOpenSelectOption}>
+              Open SelectOption_1
             </button>
           </div>
         </section>
       </main>
-      <div ref={ref_layerpop} className={`LayerPopup`}>
+      <div ref={ref_tooltip} className={`LayerPopup`}>
         <ToolTip_1 />
       </div>
-      <div ref={ref_layerpop} className={`LayerPopup`}>
+      <div ref={ref_confirm} className={`LayerPopup`}>
         <Confirm_1 />
+      </div>
+      <div ref={ref_selecttoption} className={`LayerPopup`}>
+        <SelectOption_1 />
       </div>
     </>
   );
