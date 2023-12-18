47964d462cc54f472a49c749fe756e9d300b8bf7
강석민 <dol2156@gmail.com>
2023-12-18 10:48:36 +0900

    dd

--- src/components/header/TopActionBar_1.jsx
+++ src/components/header/TopActionBar_1.jsx
@@ -2,23 +2,26 @@
 import { Fragment } from 'react';
 
 export default function TopActionBar_1() {
+  function onAsideOpenClick() {
+    const $aside = document.querySelector('#Aside');
+    $aside.classList.add('On');
+  }
+
   return (
     <>
       <div className="bg-[#ffffd4] h-[--header-hei]">
         <section className="HBox h-full">
           <div className="Inner h-full">
             <article className="RRR CenterBetween h-full">
-              <div>
+              <div className={`Fc`}>
                 <a href="/" className="block">
                   <img className="w-[100px]" src="https://gifpng.com/140x40" alt="" />
                 </a>
               </div>
-              <div>
-                <article className="CenterRight gap-[16px]">
-                  <a href="#" className="Button_2 Lg w-[149px]">
-                    Aside Open
-                  </a>
-                </article>
+              <div className={`Fc`}>
+                <button className="Button_2 Lg w-[149px]" onClick={onAsideOpenClick}>
+                  Aside Open
+                </button>
               </div>
             </article>
           </div>
