64a69fc0721acca8eb2132524d3d5d17d3de106c
강석민 <dol2156@gmail.com>
2023-12-31 16:21:57 +0900

    ddd

--- src/components/common/DataGrid_1.jsx
+++ src/components/common/DataGrid_1.jsx
@@ -4,31 +4,39 @@ import { Fragment } from 'react';
 export default function DataGrid_1() {
   return (
     <>
-      <div className={`DataGrid_1 h-[calc(36px*9)]`}>
-        <table>
-          <thead>
-            <tr>
-              <th>A</th>
-              <th>B</th>
-              <th>C</th>
-            </tr>
-          </thead>
-          <tbody>
+      <div className={`DataGrid_1`}>
+        <div className={`Inner`}>
+          <div className={`Head`}>
+            <div className={`w-[70px]`}>A</div>
+            <div className={`flex-1`}>B</div>
+            <div className={`w-[100px]`}>C</div>
+          </div>
+          <div className={`Body h-[360px]`}>
             {[...Array(100).keys()].map((obj, idx) => {
               return (
                 <Fragment key={idx}>
-                  <tr>
-                    <td>
-                      <div className={`p-[6px_12px]`}>키워드 {idx}</div>
-                    </td>
-                    <td>{idx * 10}</td>
-                    <td>{idx * 10}</td>
-                  </tr>
+                  <article className={`flex gap-[0]`}>
+                    <div className={`w-[70px]`}>{idx + 1}</div>
+                    <div className={`flex-1`}>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley of type and scrambled it to make a
+                      type specimen book. It has survived not only five
+                      centuries, but also the leap into electronic typesetting,
+                      remaining essentially unchanged. It was popularised in the
+                      1960s with the release of Letraset sheets containing Lorem
+                      Ipsum passages, and more recently with desktop publishing
+                      software like Aldus PageMaker including versions of Lorem
+                      Ipsum.
+                    </div>
+                    <div className={`w-[100px]`}>{idx * 10}</div>
+                  </article>
                 </Fragment>
               );
             })}
-          </tbody>
-        </table>
+          </div>
+        </div>
       </div>
     </>
   );
